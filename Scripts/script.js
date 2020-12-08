7ca2875f08783dd7a83adab67fd8c09a78f099a4
Soumyo78 <soumyo78@yahoo.com>
2020-12-09 00:29:06 +0530

    first commit

---
+++ Scripts/script.js
@@ -0,0 +1,29 @@
+function onMouseOverHome(){
+    document.getElementById('canvas').style.backgroundImage = "url('https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')";
+    document.getElementById('canvas').style.backgroundRepeat = "no-repeat";
+    document.getElementById('canvas').style.backgroundSize = "cover";
+}
+
+function onMouseOverMenu(){
+    document.getElementById('canvas').style.backgroundImage = "url('https://images.pexels.com/photos/461428/pexels-photo-461428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')";
+    document.getElementById('canvas').style.backgroundRepeat = "no-repeat";
+    document.getElementById('canvas').style.backgroundSize = "cover";
+}
+
+function onMouseOverFeedback(){
+    document.getElementById('canvas').style.backgroundImage = "url('https://images.pexels.com/photos/132694/pexels-photo-132694.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')";
+    document.getElementById('canvas').style.backgroundRepeat = "no-repeat";
+    document.getElementById('canvas').style.backgroundSize = "cover";
+}
+
+function onMouseOverAbout(){
+    document.getElementById('canvas').style.backgroundImage = "url('https://images.pexels.com/photos/958546/pexels-photo-958546.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')";
+    document.getElementById('canvas').style.backgroundRepeat = "no-repeat";
+    document.getElementById('canvas').style.backgroundSize = "cover";
+}
+
+function onMouseOut(){
+    document.getElementById('canvas').style.backgroundImage = "url('https://images.pexels.com/photos/326279/pexels-photo-326279.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')";
+    document.getElementById('canvas').style.backgroundRepeat = "no-repeat";
+    document.getElementById('canvas').style.backgroundSize = "cover";
+}
